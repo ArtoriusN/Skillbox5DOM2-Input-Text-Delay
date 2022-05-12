f637ae55e24c5e12a3e240052a4b558bd938b17d
Artur Saiakhov <pokermenu@gmail.com>
2022-05-12 14:54:36 +0300

    removed the superfluous and led to the condition

--- timeout.js
+++ timeout.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     let h2 = document.createElement('h2');
     h2.className = "header";
-    h2.textContent = "Empty";
     document.body.prepend(h2);
     let input = document.createElement('input');
     input.className = "input";
     document.body.prepend(input);
     let h2content = document.querySelector('.header');
-    console.log(h2content);
     let timerId;
 
     function changeTagDelay() {
